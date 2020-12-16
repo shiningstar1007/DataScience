e8181b728e219e10019016ff06219f0574961288
DESKTOP-04UB7JM\yunseong <blackreport5@gmail.com>
2020-12-16 19:06:30 +0900

    Function, Module

---
+++ Code/Python_Module.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+def Python_Module_Test1():
+    print("Python_Module_Test1")
+    
+def Python_Module_Test2(num1, num2):
+    retNum = 0
+    
+    retNum = num1 + num2
+    
+    return retNum
+
